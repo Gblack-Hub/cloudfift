a486562df906b6adb88060de0a2f6b63fe8b40c5
Gblack-Hub <oladiranwale@gmail.com>
2022-01-20 18:11:22 +0100

    first commit, add and export all assets

---
+++ src/utils/exports/assets.ts
@@ -0,0 +1,88 @@
+import { ReactComponent as Logo } from "../.././assets/svgs/getajob-logo.svg";
+import { ReactComponent as Location } from "../.././assets/svgs/location.svg";
+import { ReactComponent as Search } from "../.././assets/svgs/search.svg";
+
+import Hero from "../.././assets/images/hero-image.png";
+
+import LogoWithTItle from "../.././assets/svgs/logo.svg";
+
+import PayStackMuted from "../.././assets/svgs/paystack-muted.svg";
+import GooggleMuted from "../.././assets/svgs/google-muted.svg";
+import KPMGMuted from "../.././assets/svgs/kpmg-muted.svg";
+
+import PayStack from "../.././assets/svgs/paystack.svg";
+import Google from "../.././assets/svgs/google.svg";
+import Hr from "../.././assets/svgs/hr-manager.svg";
+import AccessBank from "../.././assets/svgs/access-bank.svg";
+
+import ProductAnalyst from "../.././assets/images/img-1.png";
+import HrManager from "../.././assets/images/img-4.png";
+import SecurityTrader from "../.././assets/images/img-2.png";
+
+import Check from "../.././assets/svgs/check.svg";
+
+import OilAndGas from "../.././assets/svgs/oil-and-gas.svg";
+import HealthCare from "../.././assets/svgs/health-care.svg";
+import Education from "../.././assets/svgs/education.svg";
+import Accounting from "../.././assets/svgs/accounting.svg";
+import DesingArt from "../.././assets/svgs/art-design.svg";
+import Laptop from "../.././assets/svgs/laptop.svg";
+import Security from "../.././assets/svgs/security.svg";
+import DataAnalyst from "../.././assets/svgs/data-analyst.svg";
+
+import JobNetworkLogos from "../.././assets/images/logos.png";
+import { ReactComponent as RightArrowSmall } from "../.././assets/svgs/right-arrow.svg";
+
+import TestimonyImg from "../.././assets/images/img-5.png";
+
+import { ReactComponent as NextArrow } from "../.././assets/svgs/next-arrow.svg";
+import { ReactComponent as PrevArrow } from "../.././assets/svgs/prev-arrow.svg";
+
+import { ReactComponent as Instagram } from "../.././assets/svgs/instagram.svg";
+import { ReactComponent as Facebook } from "../.././assets/svgs/facebook.svg";
+import { ReactComponent as Twitter } from "../.././assets/svgs/twitter.svg";
+import { ReactComponent as LinkedIn } from "../.././assets/svgs/linkedin.svg";
+
+import { ReactComponent as MenuBar } from "../.././assets/svgs/menu_bar.svg";
+import { ReactComponent as Cancel } from "../.././assets/svgs/cancel.svg";
+
+import LogoImage from "../.././assets/svgs/getajob-logo.svg";
+
+export {
+  LogoImage,
+  Cancel,
+  MenuBar,
+  Hero,
+  LogoWithTItle,
+  Instagram,
+  Facebook,
+  Twitter,
+  LinkedIn,
+  NextArrow,
+  PrevArrow,
+  TestimonyImg,
+  RightArrowSmall,
+  JobNetworkLogos,
+  OilAndGas,
+  HealthCare,
+  Education,
+  Accounting,
+  DesingArt,
+  Laptop,
+  Security,
+  DataAnalyst,
+  Logo,
+  Location,
+  Search,
+  PayStackMuted,
+  GooggleMuted,
+  KPMGMuted,
+  PayStack,
+  Google,
+  Hr,
+  AccessBank,
+  ProductAnalyst,
+  HrManager,
+  SecurityTrader,
+  Check,
+};
